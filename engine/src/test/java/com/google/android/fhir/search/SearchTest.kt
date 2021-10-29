57f36c185680abf17ee78bf757513a200a917430
Jing Tang <jingtang@google.com>
2021-10-29 13:34:47 +0100

    Always canonicalize quantity values

--- engine/src/test/java/com/google/android/fhir/search/SearchTest.kt
+++ engine/src/test/java/com/google/android/fhir/search/SearchTest.kt
@@ -2031,7 +2031,7 @@ class SearchTest {
         AND a.resourceId IN (
         SELECT resourceId FROM QuantityIndexEntity
         WHERE resourceType= ? AND index_name = ?
-        AND index_system = ? AND (index_code = ? AND index_value >= ? AND index_value < ? OR index_canonicalCode = ? AND index_canonicalValue >= ? AND index_canonicalValue < ?)
+        AND index_system = ? AND index_code = ? AND index_value >= ? AND index_value < ?
         )
         """.trimIndent()
       )
@@ -2042,9 +2042,6 @@ class SearchTest {
           ResourceType.Observation.name,
           Observation.VALUE_QUANTITY.paramName,
           "http://unitsofmeasure.org",
-          "mg",
-          BigDecimal("5402.5").toDouble(),
-          BigDecimal("5403.5").toDouble(),
           "g",
           BigDecimal("5.4025").toDouble(),
           BigDecimal("5.4035").toDouble()
